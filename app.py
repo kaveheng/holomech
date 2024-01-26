d705f965d383987675cea2455be14f9bf906b041
kaveheng <58515519+kaveheng@users.noreply.github.com>
2024-01-26 13:51:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, this new!'
+@app.route('/sum', methods=['POST'])
+def calculate_sum():
+    data = request.get_json()
+    sum_result = data['number1'] + data['number2']
+    return jsonify({'sum': sum_result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
